6e2c142050b3a90602c087873ff964aee6fedd39
adityagoel <agoel25@student.ubc.ca>
2024-07-08 23:28:11 -0700

    added comment

--- include/matching/engine.h
+++ include/matching/engine.h
@@ -16,19 +16,14 @@ struct OrderBookHandler {
     explicit OrderBookHandler(std::unique_ptr<EventHandler> event_handler);
 
     void addOrderBook(uint32_t symbol_id, std::string symbol_name);
-
     void deleteOrderBook(uint32_t symbol_id, std::string symbol_name);
 
+    // order functions with additional symbol_id to identify the order_book it is a part of
     void addOrder(const Order &order);
-
     void deleteOrder(uint32_t symbol_id, uint64_t order_id);
-
     void cancelOrder(uint32_t symbol_id, uint64_t order_id, uint64_t cancelled_quantity);
-
     void modifyOrder(uint32_t symbol_id, uint64_t order_id, uint64_t new_order_id, uint64_t new_price);
-
     void executeOrder(uint32_t symbol_id, uint64_t order_id, uint64_t quantity, uint64_t price);
-
     void executeOrder(uint32_t symbol_id, uint64_t order_id, uint64_t quantity);
 
     std::string toString();
